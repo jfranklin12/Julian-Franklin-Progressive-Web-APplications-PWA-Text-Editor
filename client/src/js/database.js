7148344aacbb77df2fd9421d6372bb914fce998c
jfranklin12 <j.franks725@gmail.com>
2022-08-17 16:01:16 -0400

    scripts added and database started

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,21 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readwrite');
+  const store = tx.objextStore('jate');
+  const request = store.add({ todo: content });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log('GET from the database');
+  const todosDb = await openDB('jate', 1);
+  const tx = todosDb.transaction('jate', 'readonly');
+  const 
 
 initdb();
